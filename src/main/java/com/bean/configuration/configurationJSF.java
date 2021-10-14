27f46a5eddd66f614aeee0a1653b5e2762a8ddc7
David Rojas <david154x@hotmail.com>
2021-10-13 22:19:08 -0500

    Curso JSF con global mentoring completoado hasta la seccion 72

---
+++ src/main/java/com/bean/configuration/configurationJSF.java
@@ -0,0 +1,11 @@
+package com.bean.configuration;
+
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+
+@FacesConfig(
+		version = Version.JSF_2_3
+		)
+public class configurationJSF {
+
+}
